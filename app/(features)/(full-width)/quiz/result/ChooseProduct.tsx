dbc0960dd6c3edca1e1708f44d1ba40781f76c7c
chornsovanpanha <amyjohn922@gmail.com>
2025-09-22 19:56:21 +0700

    feat: search dialog home and quiz

--- app/(features)/(full-width)/quiz/result/ChooseProduct.tsx
+++ app/(features)/(full-width)/quiz/result/ChooseProduct.tsx
@@ -1,4 +1,11 @@
+import RoutineBuilderDialog from "@/components/my-routine/RoutineBuilderDialog";
+import SearchArea from "@/components/SearchArea";
 import { Button } from "@/components/ui/button";
+import { Dialog } from "@/components/ui/dialog";
+import { SearchType } from "@/types";
+import { popularProductListings } from "@/utils/mock";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 const ChooseProduct = ({
   currentIndex,
@@ -7,14 +14,49 @@ const ChooseProduct = ({
   currentIndex: number;
   onContinue: () => void;
 }) => {
+  const pathName = usePathname();
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const [searchAreaKey, setSearchAreaKey] = useState(0);
+  const [open, setOpen] = useState(false);
+  const clearSingleParam = () => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.delete("q");
+    router.push(`${pathName}?${params.toString()}`);
+  };
+  const onCloseDialog = () => {
+    setOpen(false);
+    setSearchAreaKey((pre) => pre + 1);
+    clearSingleParam();
+  };
+
+  function handleTapItem(type: SearchType): void {
+    console.log(type);
+    setOpen(true);
+  }
   return (
-    <section className="result flex flex-col items-center space-y-4">
-      <Button
-        onClick={onContinue}
-        className="w-[50%] bg-secondary hover:bg-secondary/90 text-primary rounded-2xl"
-      >
-        I have finished adding my skincare routine {currentIndex}
-      </Button>
+    <section className="result flex flex-col  space-y-4">
+      <Dialog onOpenChange={setOpen} open={open}>
+        <SearchArea
+          key={searchAreaKey}
+          showRecent={false}
+          products={popularProductListings}
+          className="bg-white w-full  border border-gray-5 placeholder:text-gray-5 text-gray-6"
+          handleTapItem={handleTapItem}
+        />
+        <Button
+          onClick={onContinue}
+          type="button"
+          className="w-[100%] bg-secondary hover:bg-secondary/90 text-primary rounded-2xl"
+        >
+          I have finished adding my skincare routine {currentIndex}
+        </Button>
+        <RoutineBuilderDialog
+          onAdd={onCloseDialog}
+          onClose={onCloseDialog}
+          onRemove={onCloseDialog}
+        />
+      </Dialog>
     </section>
   );
 };
--- components/landing-page/SearchPreviewListing.tsx
+++ components/landing-page/SearchPreviewListing.tsx
@@ -1,31 +1,37 @@
-import { SearchType } from "@/types";
-import { searchPreviewListing } from "@/utils/mock";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Ingredient, Product, SearchType } from "@/types";
 import IngredientListItem from "../IngredientListItem";
 import SmallProductItem from "../SmallProductItem";
 
 const SearchPreviewListing = ({
   onPress,
+  products,
+  ingredients,
 }: {
   onPress: (type: SearchType) => void;
+  products?: Product[];
+  ingredients?: Ingredient[];
 }) => {
   return (
     <div className="box-area absolute top-18 left-0 right-0 bg-white rounded-none rounded-b-2xl z-10 shadow pt-6">
-      {searchPreviewListing?.ingredients?.map((ingred) => (
-        <IngredientListItem
-          data={ingred}
-          key={ingred.id}
-          onPress={() => onPress("ingredient")}
-        />
-      ))}
-      {searchPreviewListing?.products?.map((product) => (
-        <SmallProductItem
-          showBrand={true}
-          product={product}
-          type="routine"
-          onPress={() => onPress("product")}
-          key={product.id}
-        />
-      ))}
+      <ScrollArea className="h-[400px]">
+        {ingredients?.map((ingred) => (
+          <IngredientListItem
+            data={ingred}
+            key={ingred.id}
+            onPress={() => onPress("ingredient")}
+          />
+        ))}
+        {products?.map((product) => (
+          <SmallProductItem
+            showBrand={true}
+            product={product}
+            type="routine"
+            onPress={() => onPress("product")}
+            key={product.id}
+          />
+        ))}
+      </ScrollArea>
     </div>
   );
 };
