15671530190271994758bf68f13552901fe20985
chornsovanpanha <amyjohn922@gmail.com>
2025-10-21 10:57:45 +0700

    fix query issue slice 15 charc

--- app/(features)/(main)/product/[name]/ProductDetail.tsx
+++ app/(features)/(main)/product/[name]/ProductDetail.tsx
@@ -71,7 +71,11 @@ const ProductDetail = ({
 
             <footer className="w-full flex gap-2 items-center">
               <Link
-                href={userId ? `/my-routine?search=${product?.name}` : "/login"}
+                href={
+                  userId
+                    ? `/my-routine?search=${product?.name?.slice(0, 15)}`
+                    : "/login"
+                }
                 className="rounded-full bg-primary text-secondary px-8 hover:bg-primary/80 py-2.5"
               >
                 Add to routine
